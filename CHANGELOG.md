91191eb6fd3d77e002c7a3934eba601062251833
Yves Vogl <yves.vogl@adesso-service.com>
2020-11-01 13:40:38 +0100

    Initial release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,3 @@
 # Changelog
 
 Note: version releases in the 0.x.y range may introduce breaking changes.
-
-## 1.0.0
-
-- Initial Release
--- pipe/test.py
+++ pipe/test.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: Use pytest
-
 from unittest.mock import Mock, patch
 from pathlib import Path
 import os
